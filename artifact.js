4d0ecf30f6deeae18bf5252a0275aff4d7642653
luciarebolino <87474546+luciarebolino@users.noreply.github.com>
2022-10-03 19:29:26 -0400

    Add files via upload

---
+++ artifact.js
@@ -0,0 +1,22 @@
+function changeImage() {
+
+    if (document.getElementById("imgClickAndChange").src == "https://lh3.googleusercontent.com/HbNDuA_6ZZLbhYL6a5Ax6yP1-sPC2Hut4E7W5QdUUqUgURKj9wKVfu-T6yMVToKunQNhPky5ujCc7hLGrd8teJbVx5kobCbN83mbcZYkYF1u29jk-33quhFE1r9goHLhnLotDvfM=w2400") 
+    {
+        document.getElementById("imgClickAndChange").src = "https://lh3.googleusercontent.com/0bEZ0bCUYOySTXkHSZLnKojbWMEZ8LZ0-w1fksFPHGVxmKSNufDbZqJXwh33lKBF36MNX1NEoc_J_vRXdEm5RbCt0R7nc3cflOv1NuD4Y1w-rQpdQH2nrhVFBm30U0SAMuQVVc4w=w2400";
+
+    }
+    else if (document.getElementById("imgClickAndChange").src == "https://lh3.googleusercontent.com/0bEZ0bCUYOySTXkHSZLnKojbWMEZ8LZ0-w1fksFPHGVxmKSNufDbZqJXwh33lKBF36MNX1NEoc_J_vRXdEm5RbCt0R7nc3cflOv1NuD4Y1w-rQpdQH2nrhVFBm30U0SAMuQVVc4w=w2400")
+    {
+        document.getElementById("imgClickAndChange").src = "https://lh3.googleusercontent.com/p-HRa9cHIhxFHRvgGMw0dfbZFfFMfFW3Pgzdpd_yStS0-Raq7JxWBM5YaZeAeBR0qN2nMwN3Prioun9fI7itkleU1cChzpVWV1kSahxhvCVeBTbpnIYGa2Pz9-YHhiLFIL9ndHXIhg=w2400";
+
+    }
+    else if(document.getElementById("imgClickAndChange").src == "https://lh3.googleusercontent.com/p-HRa9cHIhxFHRvgGMw0dfbZFfFMfFW3Pgzdpd_yStS0-Raq7JxWBM5YaZeAeBR0qN2nMwN3Prioun9fI7itkleU1cChzpVWV1kSahxhvCVeBTbpnIYGa2Pz9-YHhiLFIL9ndHXIhg=w2400"){
+    document.getElementById("imgClickAndChange").src = "https://lh3.googleusercontent.com/6PxSamMIBVGnJHUe9QfjbmPv4-onqjhKSbjhGaLiNpZFHu-Q2yujxlxeD0Vs2n7EvETlgidhXGQDrPKiXF2G5GuTawzczGzwLsXpa7dJL53eREn3bmcF1Bxxa4O3XtQdRI2as_durA=w2400";
+    }
+
+
+else if(document.getElementById("imgClickAndChange").src == "https://lh3.googleusercontent.com/6PxSamMIBVGnJHUe9QfjbmPv4-onqjhKSbjhGaLiNpZFHu-Q2yujxlxeD0Vs2n7EvETlgidhXGQDrPKiXF2G5GuTawzczGzwLsXpa7dJL53eREn3bmcF1Bxxa4O3XtQdRI2as_durA=w2400"){
+document.getElementById("imgClickAndChange").src = "https://lh3.googleusercontent.com/41QijMz7GygsrDjWcnSPOtSkCxNlALMCry_KZyLDBakjQiN3sfIf6HHRBpEbj6vA-z1tCgpwsr5NawLL3RL2tUlAITvO7_NljgmTJ1DtuYNNgPBTcrhzGqt5zZ1ITjII9AAKHV92dA=w2400";
+}
+
+}
